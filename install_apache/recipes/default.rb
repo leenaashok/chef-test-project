b427c06728ee084018887c9650856e2cb36c833c
leena <leenaashok27@gmail.com>
2020-01-07 13:45:28 +0000

    add text comment

---
+++ install_apache/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: install_apache
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+  execute "update-upgrade" do
+  	command "sudo apt-get update && sudo apt-get upgrade -y"
+  	action :run
+  end
+# This is a testing text.
